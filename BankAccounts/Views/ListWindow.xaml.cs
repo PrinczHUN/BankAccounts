5e41a26a173956f10fb0b9c995fe701b5587c5ea
Princzes Daniel <princz98@gmail.com>
2022-02-06 17:39:07 +0100

    listing with datetime

--- BankAccounts/Views/ListWindow.xaml.cs
+++ BankAccounts/Views/ListWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Windows;
 using System.Data;
 using BankAccounts.Models;
+using System.Windows.Controls;
+using System;
 
 namespace BankAccounts.Views
 {
@@ -23,14 +25,30 @@ namespace BankAccounts.Views
             }
         }
 
-        private void listTransactionsClick(object sender, RoutedEventArgs e)
+        private void printListClick(object sender, RoutedEventArgs e)
         {
-            dataGrid.ItemsSource = transactions;
+            
         }
 
-        private void listPaymentsClick(object sender, RoutedEventArgs e)
+        private void listClick(object sender, RoutedEventArgs e)
         {
-            dataGrid.ItemsSource = payments;
+            string btnText = ((Button)sender).Content.ToString();
+            var start = dateStartPicker.SelectedDate;
+            var end = dateEndPicker.SelectedDate;
+            if(start != null && end != null)
+            {
+                if(btnText.Split(" ").Last() == "Transactions")
+                    dataGrid.ItemsSource = transactions.Where(x => x.DateTime < end && x.DateTime > start);
+                else
+                    dataGrid.ItemsSource = payments.Where(x => x.DateTime < end && x.DateTime > start);
+            }
+            else
+            {
+                if (btnText.Split(" ").Last() == "Transactions")
+                    dataGrid.ItemsSource = transactions;
+                else
+                    dataGrid.ItemsSource = payments;
+            }
         }
     }
 }
